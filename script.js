c966c46199dda3711524f2f5ccf692a6e4a56377
Johni Cassere <johnicassere@hotmail.com>
2021-11-01 00:28:43 -0300

    Atualizando Front

---
+++ script.js
@@ -0,0 +1,144 @@
+const lista = document.getElementById('lista');
+const apiUrl = 'http://localhost:3000/albuns';
+
+let titulo = document.getElementById('titulo');
+let artista = document.getElementById('artista');
+let imagem = document.getElementById('capa');
+let genero = document.getElementById('genero');
+let nota = document.getElementById('nota');
+
+let edicao = false;
+let idEdicao = 0;
+
+//GET
+const getAlbuns = async () => {
+    
+    const response = await fetch(apiUrl)
+    
+    const albuns = await response.json();
+
+    albuns.map((album) => {
+        lista.insertAdjacentHTML('beforeend', `
+        <div class="col">
+            <div class="card">
+             <img src="${album.imagem}" class="card-img-top" alt="..."> 
+                <div class="card-body">
+                <h5 class="card-title">${album.titulo} - ${album.artista}</h5>
+                <span class="badge bg-primary">${album.genero}</span>
+                <p class="card-text"> &#9733;${album.nota}</p>
+                
+                  <div>
+                     <button class="btn btn-primary" onclick="editAlbum('${album.id}')">Editar</button>
+                     <button class="btn btn-danger" onclick="deleteAlbum('${album.id}')">Excluir</button>
+                 </div>
+               </div>
+            </div>
+        </div>
+        `)
+    })
+}
+//POST
+const submitForm = async (event) => {
+    
+    event.preventDefault();
+
+    const album = {
+        titulo: titulo.value,
+        artista: artista.value,
+        imagem: imagem.value,
+        genero: genero.value,
+        nota: parseInt(nota.value),  
+    }
+    
+
+    if(edicao) {
+        putAlbum(album, idEdicao);
+    } else {
+        criarAlbum(album);
+    }
+
+    clearFields();
+    lista.innerHTML = '';
+}
+
+const criarAlbum = async(album) => {
+    const request = new Request(`${apiUrl}/add`, {
+        method: 'POST',
+        body: JSON.stringify(album),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    })
+
+    const response = await fetch(request);
+
+    const result = await response.json();
+    
+    alert(result.message)
+  
+    getAlbuns();
+
+}
+//PUT
+const putAlbum = async(album, id) => {
+    
+    const request = new Request(`${apiUrl}/edit/${id}`, {
+        method:  'PUT',
+        body: JSON.stringify(album),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    })
+    const response = await fetch(request);
+    const result = await response.json();
+    
+    alert(result.message)
+    edicao = false;
+    idEdicao = 0;
+    getAlbuns();
+}
+
+// [DELETE] funcao que exclui 
+const deleteAlbum = async (id) => {
+    
+    const request = new Request(`${apiUrl}/delete/${id}`, {
+        method: 'DELETE'
+    })
+
+    const response = await fetch(request);
+    const result = await response.json();
+
+    alert(result.message);
+    
+    lista.innerHTML = '';
+    getAlbuns();
+}
+
+// [GET] {id} - funcao onde recebe um id via paramtero 
+const getAlbumById = async (id) => {
+    const response = await fetch(`${apiUrl}/${id}`);
+    return await response.json();
+}
+
+const editAlbum = async (id) => {
+    edicao = true;
+    idEdicao = id;
+
+    const album = await getAlbumById(id);
+
+    titulo.value = album.titulo;
+    artista.value =  album.artista;
+    imagem.value = album.imagem;
+    genero.value = album.genero;
+    nota.value = album.nota;
+}
+
+const clearFields = () => {
+    titulo.value = '';
+    artista.value = '';
+    imagem.value = '';
+    genero.value = '';
+    nota.value = '';
+}
+
+getAlbuns();
